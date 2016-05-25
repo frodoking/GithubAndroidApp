c3e00e453c0f93593156349a653c3a3ad38d8c46
frodok <awangyun8@gmail.com>
2016-05-25 11:57:42 +0800

    improve repository

--- app/src/main/java/com/frodo/github/business/repository/RepositoryView.java
+++ app/src/main/java/com/frodo/github/business/repository/RepositoryView.java
@@ -1,10 +1,8 @@
 package com.frodo.github.business.repository;
 
-import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -12,8 +10,8 @@ import android.widget.TextView;
 
 import com.frodo.app.android.core.AndroidUIViewController;
 import com.frodo.app.android.core.UIView;
+import com.frodo.app.android.core.toolbox.DrawableHelper;
 import com.frodo.app.android.core.toolbox.ResourceManager;
-import com.frodo.github.DrawableHelper;
 import com.frodo.github.R;
 import com.frodo.github.bean.Repository;
 import com.frodo.github.view.CardViewGroup;
@@ -38,6 +36,14 @@ public class RepositoryView extends UIView {
     private TextView forksTV;
 
     private CardViewGroup branchsCVG;
+    private TextView branchsTV;
+    private TextView branchCommitTV;
+
+    private CardViewGroup readmeCVG;
+    private CardViewGroup pulseCVG;
+    private CardViewGroup issuesCVG;
+    private CardViewGroup pullRequestsCVG;
+    private CardViewGroup notificationsCVG;
 
     public RepositoryView(AndroidUIViewController presenter, LayoutInflater inflater, ViewGroup container) {
         super(presenter, inflater, container, R.layout.fragment_repository);
@@ -60,18 +66,98 @@ public class RepositoryView extends UIView {
         watchersTV = (TextView) getRootView().findViewById(R.id.watchers_tv);
         forksTV = (TextView) getRootView().findViewById(R.id.forks_tv);
 
+        initBranchsCardView();
+        initReadmeCardView();
+        initPulseCardView();
+        initIssuesCardView();
+        initPullRequestsCardView();
+        initNotificationsCardView();
+    }
+
+    private void initBranchsCardView() {
         branchsCVG = (CardViewGroup) getRootView().findViewById(R.id.branchs_cvg);
 
+        branchsTV = (TextView) branchsCVG.getHeaderView().findViewById(R.id.title_tv);
+        TextView branchsSubTV = (TextView) branchsCVG.getHeaderView().findViewById(R.id.subtitle_tv);
+        branchsSubTV.setText("");
+        branchsTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_git_branch), null, null, null);
+        branchsSubTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_chevron_down), null, null, null);
+
+        LinearLayout ll = (LinearLayout) branchsCVG.getContentView();
+        branchCommitTV = new TextView(getPresenter().getAndroidContext());
+        branchCommitTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        branchCommitTV.setGravity(Gravity.CENTER_VERTICAL);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ResourceManager.getDimensionPixelSize(R.dimen.item_height_default));
+        int margin = ResourceManager.getDimensionPixelSize(R.dimen.margin_middle);
+        lp.leftMargin = margin;
+        lp.rightMargin = margin;
+        ll.addView(branchCommitTV, lp);
+
+        branchsCVG = (CardViewGroup) getRootView().findViewById(R.id.branchs_cvg);
         TextView viewCodeTV = (TextView) branchsCVG.getFooterView().findViewById(R.id.repo_view_code_tv);
         TextView jumpToFileTV = (TextView) branchsCVG.getFooterView().findViewById(R.id.repo_jump_to_file_tv);
         viewCodeTV.setCompoundDrawablesRelativeWithIntrinsicBounds(
                 DrawableHelper.withContext(getPresenter().getAndroidContext())
-                .withColor(R.color.colorPrimary)
-                .withDrawable(R.drawable.octicon_file_directory).tint().get(), null,null,null);
+                        .withColor(R.color.colorPrimary)
+                        .withDrawable(R.drawable.octicon_file_directory).tint().get(), null, null, null);
         jumpToFileTV.setCompoundDrawablesRelativeWithIntrinsicBounds(
                 DrawableHelper.withContext(getPresenter().getAndroidContext())
-                .withColor(R.color.colorPrimary)
-                .withDrawable(R.drawable.octicon_search).tint().get(), null,null,null);
+                        .withColor(R.color.colorPrimary)
+                        .withDrawable(R.drawable.octicon_search).tint().get(), null, null, null);
+    }
+
+    private void initReadmeCardView() {
+        readmeCVG = (CardViewGroup) getRootView().findViewById(R.id.readme_cvg);
+
+        TextView titleTV = (TextView) readmeCVG.getHeaderView().findViewById(R.id.title_tv);
+        titleTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_book), null, null, null);
+        titleTV.setText("README.md");
+
+        TextView footTV = (TextView) readmeCVG.getFooterView().findViewById(R.id.text_tv);
+        footTV.setText("View all of README.md");
+    }
+
+    private void initPulseCardView() {
+        pulseCVG = (CardViewGroup) getRootView().findViewById(R.id.pulse_cvg);
+
+        TextView titleTV = (TextView) pulseCVG.getHeaderView().findViewById(R.id.title_tv);
+        TextView subtitleTVTV = (TextView) pulseCVG.getHeaderView().findViewById(R.id.subtitle_tv);
+        titleTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_pulse), null, null, null);
+        titleTV.setText("Pulse");
+        subtitleTVTV.setText("Past week");
+
+        TextView footTV = (TextView) pulseCVG.getFooterView().findViewById(R.id.text_tv);
+        footTV.setText("View Pulse");
+    }
+
+    private void initIssuesCardView() {
+        issuesCVG = (CardViewGroup) getRootView().findViewById(R.id.issues_cvg);
+
+        TextView titleTV = (TextView) issuesCVG.getHeaderView().findViewById(R.id.title_tv);
+        titleTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_issue_opened), null, null, null);
+        titleTV.setText("Issues");
+
+        TextView footTV = (TextView) issuesCVG.getFooterView().findViewById(R.id.text_tv);
+        footTV.setText("View all issues");
+    }
+
+    private void initPullRequestsCardView() {
+        pullRequestsCVG = (CardViewGroup) getRootView().findViewById(R.id.pull_requests_cvg);
+
+        TextView titleTV = (TextView) pullRequestsCVG.getHeaderView().findViewById(R.id.title_tv);
+        titleTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_git_pull_request), null, null, null);
+        titleTV.setText("Pull Requests");
+
+        TextView footTV = (TextView) pullRequestsCVG.getFooterView().findViewById(R.id.text_tv);
+        footTV.setText("View all pull requests");
+    }
+
+    private void initNotificationsCardView() {
+        notificationsCVG = (CardViewGroup) getRootView().findViewById(R.id.notifications_cvg);
+
+        TextView titleTV = (TextView) notificationsCVG.getHeaderView().findViewById(R.id.title_tv);
+        titleTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_bell), null, null, null);
+        titleTV.setText("Notifications");
     }
 
     @Override
@@ -80,7 +166,7 @@ public class RepositoryView extends UIView {
 
     public void showDetail(Repository repository) {
         descriptionTV.setText(repository.description);
-        issuesCountTV.setText(String.format("%s issues",repository.open_issues_count));
+        issuesCountTV.setText(String.format("%s issues", repository.open_issues_count));
         languageTV.setText(repository.language);
         sizeTV.setText(String.format("%s KB", repository.size));
         dateTV.setText(repository.created_at.toLocaleString());
@@ -91,22 +177,7 @@ public class RepositoryView extends UIView {
         watchersTV.setText(String.valueOf(repository.subscribers_count));
         forksTV.setText(String.valueOf(repository.forks_count));
 
-        TextView branchsTV = (TextView) branchsCVG.getHeaderView().findViewById(R.id.title_tv);
-        TextView branchsSubTV = (TextView) branchsCVG.getHeaderView().findViewById(R.id.subtitle_tv);
         branchsTV.setText(repository.default_branch);
-        branchsSubTV.setText("");
-        branchsTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_git_branch),null,null,null);
-        branchsSubTV.setCompoundDrawablesWithIntrinsicBounds(ResourceManager.getDrawable(R.drawable.octicon_chevron_down),null,null,null);
-
-        LinearLayout ll = (LinearLayout) branchsCVG.getContentView();
-        TextView textView = new TextView(getPresenter().getAndroidContext());
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
-        textView.setText("Latest commit by frodoking 7 days ago");
-        textView.setGravity(Gravity.CENTER_VERTICAL);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ResourceManager.getDimensionPixelSize(R.dimen.item_height_default));
-        int margin  = ResourceManager.getDimensionPixelSize(R.dimen.margin_middle);
-        lp.leftMargin = margin;
-        lp.rightMargin = margin;
-        ll.addView(textView, lp);
+        branchCommitTV.setText("Latest commit by frodoking 7 days ago");
     }
 }
