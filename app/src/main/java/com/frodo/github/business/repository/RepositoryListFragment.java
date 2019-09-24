94d72106c88d86f8f86eaf9bae7d1d874365190b
frodo <awangyun8@gmail.com>
2019-09-24 15:29:23 +0800

    Update RepositoryListFragment.java

--- app/src/main/java/com/frodo/github/business/repository/RepositoryListFragment.java
+++ app/src/main/java/com/frodo/github/business/repository/RepositoryListFragment.java
@@ -12,88 +12,97 @@ import com.frodo.github.view.ViewProvider;
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by frodo on 2016/6/1.
  */
-public class RepositoryListFragment extends SearchListFragment<RepositoryModel, Repo> {
+public class RepositoryListFragment extends SearchListFragment<RepositoryModel, Repo>
+{
 
-	@Override
-	protected RepositoryModel createModel() {
-		return getMainController().getModelFactory().getOrCreateIfAbsent(RepositoryModel.TAG, RepositoryModel.class, getMainController());
-	}
+    @Override protected RepositoryModel createModel()
+    {
+        return getMainController().getModelFactory()
+                .getOrCreateIfAbsent(RepositoryModel.TAG, RepositoryModel.class, getMainController());
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tag());
-	}
+    @Override public void onResume()
+    {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tag());
+    }
 
-	@Override
-	public String tag() {
-		return "Repositories";
-	}
+    @Override public String tag()
+    {
+        return "Repositories";
+    }
 
-	@Override
-	public BaseRecyclerViewAdapter uiViewAdapter() {
-		return new RepositoriesAdapter(getAndroidContext(), true);
-	}
+    @Override public BaseRecyclerViewAdapter uiViewAdapter()
+    {
+        return new RepositoriesAdapter(getAndroidContext(), true);
+    }
 
-	@Override
-	public void doSearch(String searchKey) {
+    @Override public void doSearch(String searchKey)
+    {
 
-	}
+    }
 
-	@Override
-	public void onFirstTimeLaunched() {
-		Bundle bundle = getArguments();
-		Observable<List<Repo>> observable = null;
-		//TODO repos_user_{username}
-		// repos_forks_{owner}_{repo}
-		if (bundle != null && bundle.containsKey("repos_args")) {
-			String[] argsArray = bundle.getString("repos_args").split("_");
+    @Override public void onFirstTimeLaunched()
+    {
+        Bundle bundle = getArguments();
+        Observable<List<Repo>> observable = null;
+        //TODO repos_user_{username}
+        // repos_forks_{owner}_{repo}
+        if (bundle != null && bundle.containsKey("repos_args"))
+        {
+            String[] argsArray = bundle.getString("repos_args").split("_");
 
-			if (argsArray[0].equalsIgnoreCase("repos")) {
-				if (argsArray[1].equalsIgnoreCase("user")) {
-					observable = getModel().loadUserRepositoriesWithReactor(argsArray[2]);
-				} else if (argsArray[1].equalsIgnoreCase("explore")) {
-					// do something
-				} else if (argsArray[1].equalsIgnoreCase("forks")) {
-					observable = getModel().loadRepositoryForksWithReactor(argsArray[2], argsArray[3]);
-				}
-			}
-		}
-		if (observable != null)
-			observable.subscribeOn(Schedulers.io())
-					.doOnSubscribe(new Action0() {
-						@Override
-						public void call() {
-							getUIView().showEmptyView();
-							CircleProgressDialog.showLoadingDialog(getAndroidContext());
-						}
-					})
-					.subscribeOn(AndroidSchedulers.mainThread())
-					.observeOn(AndroidSchedulers.mainThread())
-					.subscribe(new Action1<List<Repo>>() {
-						           @Override
-						           public void call(List<Repo> repos) {
-							           CircleProgressDialog.hideLoadingDialog();
-							           setStateBeans((ArrayList<Repo>) repos);
-							           getUIView().hideEmptyView();
-							           getUIView().showList(repos);
-						           }
-					           },
-							new Action1<Throwable>() {
-								@Override
-								public void call(Throwable throwable) {
-									CircleProgressDialog.hideLoadingDialog();
-									getUIView().showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
-								}
-							});
-	}
+            if (argsArray[0].equalsIgnoreCase("repos"))
+            {
+                if (argsArray[1].equalsIgnoreCase("user"))
+                {
+                    observable = getModel().loadUserRepositoriesWithReactor(argsArray[2]);
+                }
+                else if (argsArray[1].equalsIgnoreCase("explore"))
+                {
+                    // do something
+                }
+                else if (argsArray[1].equalsIgnoreCase("forks"))
+                {
+                    observable = getModel().loadRepositoryForksWithReactor(argsArray[2], argsArray[3]);
+                }
+            }
+        }
+        if (observable != null)
+            observable.subscribeOn(Schedulers.io()).doOnSubscribe(new Consumer<Disposable>()
+            {
+                @Override public void accept(Disposable disposable)
+                {
+                    getUIView().showEmptyView();
+                    CircleProgressDialog.showLoadingDialog(getAndroidContext());
+                }
+            }).subscribeOn(AndroidSchedulers.mainThread()).observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<List<Repo>>()
+                    {
+                        @Override public void accept(List<Repo> repos)
+                        {
+                            CircleProgressDialog.hideLoadingDialog();
+                            setStateBeans((ArrayList<Repo>) repos);
+                            getUIView().hideEmptyView();
+                            getUIView().showList(repos);
+                        }
+                    }, new Consumer<Throwable>()
+                    {
+                        @Override public void accept(Throwable throwable)
+                        {
+                            CircleProgressDialog.hideLoadingDialog();
+                            getUIView().showErrorView(
+                                    ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
+                        }
+                    });
+    }
 }
