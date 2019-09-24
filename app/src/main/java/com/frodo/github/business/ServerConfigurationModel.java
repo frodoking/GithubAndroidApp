7c94bdae53bbfb9714c3294b1d11539932c8fcfb
frodo <awangyun8@gmail.com>
2019-09-24 15:52:59 +0800

    Update ServerConfigurationModel.java

--- app/src/main/java/com/frodo/github/business/ServerConfigurationModel.java
+++ app/src/main/java/com/frodo/github/business/ServerConfigurationModel.java
@@ -1,6 +1,6 @@
 package com.frodo.github.business;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import android.annotation.SuppressLint;
 import com.frodo.app.android.core.task.AndroidFetchNetworkDataTask;
 import com.frodo.app.android.core.toolbox.JsonConverter;
 import com.frodo.app.android.core.toolbox.ResourceManager;
@@ -16,84 +16,101 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Future;
 
+import com.google.gson.reflect.TypeToken;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
-import rx.Observable;
-import rx.Subscriber;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by frodo on 16/6/19.
  */
-public class ServerConfigurationModel extends AbstractModel {
+public class ServerConfigurationModel extends AbstractModel
+{
 
-	public static final String TAG = ServerConfigurationModel.class.getSimpleName();
+    public static final String TAG = ServerConfigurationModel.class.getSimpleName();
 
-	public List<GithubLanguage> languages;
+    public List<GithubLanguage> languages;
 
-	public ServerConfigurationModel(MainController controller) {
-		super(controller);
-	}
+    public ServerConfigurationModel(MainController controller)
+    {
+        super(controller);
+    }
 
-	@Override
-	public void initBusiness() {
-		loadLanguagesWithReactor()
-				.subscribeOn(Schedulers.io())
-				.subscribe(new Action1<List<GithubLanguage>>() {
-					@Override
-					public void call(List<GithubLanguage> list) {
-						languages = list;
-					}
-				}, new Action1<Throwable>() {
-					@Override
-					public void call(Throwable throwable) {
-						languages = defaultLanguages();
-						throwable.printStackTrace();
-					}
-				});
-	}
+    @SuppressLint ("CheckResult") @Override public void initBusiness()
+    {
+        loadLanguagesWithReactor().subscribeOn(Schedulers.io()).subscribe(new Consumer<List<GithubLanguage>>()
+        {
+            @Override public void accept(List<GithubLanguage> githubLanguages)
+            {
+                languages = githubLanguages;
+            }
+        }, new Consumer<Throwable>()
+        {
+            @Override public void accept(Throwable throwable)
+            {
+                languages = defaultLanguages();
+                throwable.printStackTrace();
+            }
+        });
+    }
 
-	public List<GithubLanguage> getLanguages() {
-		return languages == null || languages.isEmpty() ? defaultLanguages() : languages;
-	}
+    public List<GithubLanguage> getLanguages()
+    {
+        return languages == null || languages.isEmpty() ? defaultLanguages() : languages;
+    }
 
-	private Observable<List<GithubLanguage>> loadLanguagesWithReactor() {
-		return Observable.create(new Observable.OnSubscribe<Response>() {
-			@Override
-			public void call(Subscriber<? super Response> subscriber) {
-				Request request = new Request.Builder()
-						.method("GET")
-						.relativeUrl(Path.Explore.LANGUAGES)
-						.build();
-				final NetworkTransport networkTransport = getMainController().getNetworkTransport();
-				networkTransport.setAPIUrl(Path.HOST_CODEHUB);
-				AndroidFetchNetworkDataTask task = new AndroidFetchNetworkDataTask(getMainController().getNetworkTransport(), request, subscriber);
-				Future f = getMainController().getBackgroundExecutor().execute(task);
-			}
-		}).flatMap(new Func1<Response, Observable<List<GithubLanguage>>>() {
-			@Override
-			public Observable<List<GithubLanguage>> call(Response response) {
-				ResponseBody rb = (ResponseBody) response.getBody();
-				try {
-					List<GithubLanguage> languages = JsonConverter.convert(rb.string(), new TypeReference<List<GithubLanguage>>() {
-					});
+    private Observable<List<GithubLanguage>> loadLanguagesWithReactor()
+    {
+        return Observable.create(new ObservableOnSubscribe<Response>()
+        {
+            @Override public void subscribe(ObservableEmitter<Response> emitter)
+            {
+                Request request = new Request.Builder().method("GET").relativeUrl(Path.Explore.LANGUAGES).build();
+                final NetworkTransport networkTransport = getMainController().getNetworkTransport();
+                networkTransport.setAPIUrl(Path.HOST_CODEHUB);
+                AndroidFetchNetworkDataTask task =
+                        new AndroidFetchNetworkDataTask(getMainController().getNetworkTransport(), request, emitter);
+                Future f = getMainController().getBackgroundExecutor().execute(task);
+            }
+        }).flatMap(new Function<Response, ObservableSource<List<GithubLanguage>>>()
+        {
+            @Override public ObservableSource<List<GithubLanguage>> apply(Response response)
+            {
+                ResponseBody rb = (ResponseBody) response.getBody();
+                try
+                {
+                    List<GithubLanguage> languages =
+                            JsonConverter.convert(rb.string(), new TypeToken<List<GithubLanguage>>()
+                            {
+                            }.getType());
 
-					if (languages == null || languages.isEmpty()) {
-						return Observable.just(defaultLanguages());
-					} else {
-						return Observable.just(languages);
-					}
-				} catch (IOException e) {
-					return Observable.just(languages);
-				}
-			}
-		});
-	}
+                    if (languages == null || languages.isEmpty())
+                    {
+                        return Observable.just(defaultLanguages());
+                    }
+                    else
+                    {
+                        return Observable.just(languages);
+                    }
+                }
+                catch (IOException e)
+                {
+                    return Observable.just(languages);
+                }
+            }
+        });
+    }
 
-	private List<GithubLanguage> defaultLanguages() {
-		String languageJson = ResourceManager.getAsset("language.json");
-		return JsonConverter.convert(languageJson, new TypeReference<List<GithubLanguage>>() {
-		});
-	}
+    private List<GithubLanguage> defaultLanguages()
+    {
+        String languageJson = ResourceManager.getAsset("language.json");
+        return JsonConverter.convert(languageJson, new TypeToken<List<GithubLanguage>>()
+        {
+        }.getType());
+    }
 }
