ddf14cad016a9d96d5a5ed3f9d33f800091d42ba
frodo <awangyun8@gmail.com>
2019-09-24 15:02:51 +0800

    Update ShowCaseDetailFragment.java

--- app/src/main/java/com/frodo/github/business/explore/ShowCaseDetailFragment.java
+++ app/src/main/java/com/frodo/github/business/explore/ShowCaseDetailFragment.java
@@ -10,83 +10,84 @@ import com.frodo.app.android.ui.fragment.StatedFragment;
 import com.frodo.github.bean.ShowCase;
 import com.frodo.github.view.CircleProgressDialog;
 import com.frodo.github.view.ViewProvider;
-
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by frodo on 2016/5/3.
  */
-public class ShowCaseDetailFragment extends StatedFragment<ShowCaseDetailView, ShowCaseDetailModel> {
-	private String tag = "";
-	private ShowCase showCase;
+public class ShowCaseDetailFragment extends StatedFragment<ShowCaseDetailView, ShowCaseDetailModel>
+{
+    private String tag = "";
 
-	@Override
-	public ShowCaseDetailView createUIView(Context context, LayoutInflater inflater, ViewGroup container) {
-		return new ShowCaseDetailView(this, inflater, container);
-	}
+    private ShowCase showCase;
 
-	@Override
-	public void onFirstTimeLaunched() {
-		Bundle bundle = getArguments();
-		if (bundle != null && bundle.containsKey("slug")) {
-			tag = bundle.getString("slug");
-			loadShowCasesWithReactor(tag);
-		}
-	}
+    @Override public ShowCaseDetailView createUIView(Context context, LayoutInflater inflater, ViewGroup container)
+    {
+        return new ShowCaseDetailView(this, inflater, container);
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tag());
-	}
+    @Override public void onFirstTimeLaunched()
+    {
+        Bundle bundle = getArguments();
+        if (bundle != null && bundle.containsKey("slug"))
+        {
+            tag = bundle.getString("slug");
+            loadShowCasesWithReactor(tag);
+        }
+    }
 
-	@Override
-	public void onSaveState(Bundle outState) {
-		outState.putParcelable("showcase", showCase);
-	}
+    @Override public void onResume()
+    {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tag());
+    }
 
-	@Override
-	public void onRestoreState(Bundle savedInstanceState) {
-		showCase = savedInstanceState.getParcelable("showcase");
-		getUIView().showShowCaseDetail(showCase);
-	}
+    @Override public void onSaveState(Bundle outState)
+    {
+        outState.putParcelable("showcase", showCase);
+    }
 
-	@Override
-	public String tag() {
-		return "Explore/" + tag;
-	}
+    @Override public void onRestoreState(Bundle savedInstanceState)
+    {
+        showCase = savedInstanceState.getParcelable("showcase");
+        getUIView().showShowCaseDetail(showCase);
+    }
 
-	private void loadShowCasesWithReactor(final String slug) {
-		getModel().loadShowCaseDetailWithReactor(slug)
-				.doOnSubscribe(new Action0() {
-					@Override
-					public void call() {
-						getUIView().showEmptyView();
-						CircleProgressDialog.showLoadingDialog(getAndroidContext());
-					}
-				})
-				.subscribeOn(AndroidSchedulers.mainThread())
-				.subscribeOn(Schedulers.io())
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(
-						new Action1<ShowCase>() {
-							@Override
-							public void call(ShowCase result) {
-								CircleProgressDialog.hideLoadingDialog();
-								ShowCaseDetailFragment.this.showCase = result;
-								getUIView().hideEmptyView();
-								getUIView().showShowCaseDetail(result);
-							}
-						},
-						new Action1<Throwable>() {
-							@Override
-							public void call(Throwable throwable) {
-								CircleProgressDialog.hideLoadingDialog();
-								getUIView().showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
-							}
-						});
-	}
+    @Override public String tag()
+    {
+        return "Explore/" + tag;
+    }
+
+    private void loadShowCasesWithReactor(final String slug)
+    {
+        getModel().loadShowCaseDetailWithReactor(slug).doOnSubscribe(new Consumer<Disposable>()
+        {
+            @Override public void accept(Disposable disposable)
+            {
+                getUIView().showEmptyView();
+                CircleProgressDialog.showLoadingDialog(getAndroidContext());
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<ShowCase>()
+        {
+            @Override public void accept(ShowCase showCase)
+            {
+
+                CircleProgressDialog.hideLoadingDialog();
+                ShowCaseDetailFragment.this.showCase = showCase;
+                getUIView().hideEmptyView();
+                getUIView().showShowCaseDetail(showCase);
+            }
+        }, new Consumer<Throwable>()
+        {
+            @Override public void accept(Throwable throwable)
+            {
+                CircleProgressDialog.hideLoadingDialog();
+                getUIView()
+                        .showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
+            }
+        });
+    }
 }
