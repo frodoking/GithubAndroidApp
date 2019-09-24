b012c843045149e2add4512665387be3a4d0ad07
frodo <awangyun8@gmail.com>
2019-09-24 15:05:12 +0800

    Update TrendingFragment.java

--- app/src/main/java/com/frodo/github/business/explore/TrendingFragment.java
+++ app/src/main/java/com/frodo/github/business/explore/TrendingFragment.java
@@ -17,149 +17,154 @@ import com.frodo.github.business.ServerConfigurationModel;
 import com.frodo.github.view.CircleProgressDialog;
 import com.frodo.github.view.ViewProvider;
 import com.mikepenz.octicons_typeface_library.Octicons;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 import java.util.List;
-
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by frodo on 16/6/10.
  */
-public class TrendingFragment extends StatedFragment<TrendingView, ExploreModel> {
-	public static final int TYPE_REPOS = 0;
-	public static final int TYPE_DEVELOPERS = 1;
-	private int currentType = TYPE_REPOS;
-	private String currentLanguage = "";
+public class TrendingFragment extends StatedFragment<TrendingView, ExploreModel>
+{
+    public static final int TYPE_REPOS = 0;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setHasOptionsMenu(true);
-	}
+    public static final int TYPE_DEVELOPERS = 1;
 
-	@Override
-	public TrendingView createUIView(Context context, LayoutInflater inflater, ViewGroup container) {
-		return new TrendingView(this, inflater, container);
-	}
+    private int currentType = TYPE_REPOS;
 
+    private String currentLanguage = "";
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Trending");
-	}
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
+    @Override public TrendingView createUIView(Context context, LayoutInflater inflater, ViewGroup container)
+    {
+        return new TrendingView(this, inflater, container);
+    }
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		super.onCreateOptionsMenu(menu, inflater);
-		inflater.inflate(R.menu.menu_trending, menu);
-		updateMenu(menu);
-	}
+    @Override public void onResume()
+    {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Trending");
+    }
 
-	private void updateMenu(Menu menu) {
-		ViewProvider.updateMenuItem(getAndroidContext(), menu, R.id.action_repositories, Octicons.Icon.oct_repo);
-		ViewProvider.updateMenuItem(getAndroidContext(), menu, R.id.action_developers, Octicons.Icon.oct_mark_github);
-	}
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_trending, menu);
+        updateMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.action_repositories:
-				currentType = TYPE_REPOS;
+    private void updateMenu(Menu menu)
+    {
+        ViewProvider.updateMenuItem(getAndroidContext(), menu, R.id.action_repositories, Octicons.Icon.oct_repo);
+        ViewProvider.updateMenuItem(getAndroidContext(), menu, R.id.action_developers, Octicons.Icon.oct_mark_github);
+    }
 
-				loadListBy(this.currentLanguage);
-				break;
-			case R.id.action_developers:
-				currentType = TYPE_DEVELOPERS;
+    @Override public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_repositories:
+                currentType = TYPE_REPOS;
 
-				loadListBy(this.currentLanguage);
-				break;
-			default:
-				break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+                loadListBy(this.currentLanguage);
+                break;
+            case R.id.action_developers:
+                currentType = TYPE_DEVELOPERS;
 
-	@Override
-	public void onFirstTimeLaunched() {
-		final ServerConfigurationModel serverConfigurationModel = getMainController()
-				.getModelFactory()
-				.getOrCreateIfAbsent(ServerConfigurationModel.TAG, ServerConfigurationModel.class, getMainController());
-		getUIView().loadLanguages(serverConfigurationModel.getLanguages());
-	}
+                loadListBy(this.currentLanguage);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	public void loadListBy(String language) {
-		this.currentLanguage = language;
+    @Override public void onFirstTimeLaunched()
+    {
+        final ServerConfigurationModel serverConfigurationModel = getMainController().getModelFactory()
+                .getOrCreateIfAbsent(ServerConfigurationModel.TAG, ServerConfigurationModel.class, getMainController());
+        getUIView().loadLanguages(serverConfigurationModel.getLanguages());
+    }
 
-		if (currentType == TYPE_REPOS) {
-			loadTrendingRepositories();
-		} else if (currentType == TYPE_DEVELOPERS) {
-			loadTrendingDevelopers();
-		}
-	}
+    public void loadListBy(String language)
+    {
+        this.currentLanguage = language;
 
-	private void loadTrendingRepositories() {
-		getModel().loadTrendingRepositoriesWithReactor2("", this.currentLanguage)
-				.doOnSubscribe(new Action0() {
-					@Override
-					public void call() {
-						getUIView().showEmptyView();
-						CircleProgressDialog.showLoadingDialog(getAndroidContext());
-					}
-				})
-				.subscribeOn(AndroidSchedulers.mainThread())
-				.subscribeOn(Schedulers.io())
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(
-						new Action1<List<Repo>>() {
-							@Override
-							public void call(List<Repo> result) {
-								CircleProgressDialog.hideLoadingDialog();
-								getUIView().hideEmptyView();
-								getUIView().showRepoList(result);
-							}
-						},
-						new Action1<Throwable>() {
-							@Override
-							public void call(Throwable throwable) {
-								CircleProgressDialog.hideLoadingDialog();
-								getUIView().showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
-							}
-						});
-	}
+        if (currentType == TYPE_REPOS)
+        {
+            loadTrendingRepositories();
+        }
+        else if (currentType == TYPE_DEVELOPERS)
+        {
+            loadTrendingDevelopers();
+        }
+    }
 
-	private void loadTrendingDevelopers() {
-		getModel().loadTrendingDevelopersWithReactor("", this.currentLanguage)
-				.doOnSubscribe(new Action0() {
-					@Override
-					public void call() {
-						getUIView().showEmptyView();
-						CircleProgressDialog.showLoadingDialog(getAndroidContext());
-					}
-				})
-				.subscribeOn(AndroidSchedulers.mainThread())
-				.subscribeOn(Schedulers.io())
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(
-						new Action1<List<User>>() {
-							@Override
-							public void call(List<User> result) {
-								CircleProgressDialog.hideLoadingDialog();
-								getUIView().hideEmptyView();
-								getUIView().showDeveloperList(result);
-							}
-						},
-						new Action1<Throwable>() {
-							@Override
-							public void call(Throwable throwable) {
-								CircleProgressDialog.hideLoadingDialog();
-								getUIView().showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
-							}
-						});
-	}
+    private void loadTrendingRepositories()
+    {
+        getModel().loadTrendingRepositoriesWithReactor2("", this.currentLanguage)
+                .doOnSubscribe(new Consumer<Disposable>()
+                {
+                    @Override public void accept(Disposable disposable)
+                    {
+                        getUIView().showEmptyView();
+                        CircleProgressDialog.showLoadingDialog(getAndroidContext());
+                    }
+                }).subscribeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<List<Repo>>()
+        {
+            @Override public void accept(List<Repo> repos)
+            {
+                CircleProgressDialog.hideLoadingDialog();
+                getUIView().hideEmptyView();
+                getUIView().showRepoList(repos);
+            }
+        }, new Consumer<Throwable>()
+        {
+            @Override public void accept(Throwable throwable)
+            {
+                CircleProgressDialog.hideLoadingDialog();
+                getUIView()
+                        .showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
+            }
+        });
+    }
+
+    private void loadTrendingDevelopers()
+    {
+        getModel().loadTrendingDevelopersWithReactor("", this.currentLanguage).doOnSubscribe(new Consumer<Disposable>()
+        {
+            @Override public void accept(Disposable disposable)
+            {
+                getUIView().showEmptyView();
+                CircleProgressDialog.showLoadingDialog(getAndroidContext());
+            }
+        }).subscribeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<List<User>>()
+        {
+            @Override public void accept(List<User> users)
+            {
+                CircleProgressDialog.hideLoadingDialog();
+                getUIView().hideEmptyView();
+                getUIView().showDeveloperList(users);
+            }
+        }, new Consumer<Throwable>()
+        {
+            @Override public void accept(Throwable throwable)
+            {
+                CircleProgressDialog.hideLoadingDialog();
+                getUIView()
+                        .showErrorView(ViewProvider.handleError(getMainController().getConfig().isDebug(), throwable));
+            }
+        });
+    }
 
 }
